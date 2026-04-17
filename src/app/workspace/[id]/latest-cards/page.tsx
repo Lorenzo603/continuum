edf3cd62e812e019e30e85014ab69a6449a91d0a
Lorenzo Furrer <lorenzo.furrer@gmail.com>
2026-04-17 18:16:03 +0200

    Latest cards view: show cards horizontally first instead of vertically first

--- src/app/workspace/[id]/latest-cards/page.tsx
+++ src/app/workspace/[id]/latest-cards/page.tsx
@@ -71,7 +71,7 @@ export default async function LatestCardsPage({ params }: LatestCardsPageProps) 
               </p>
             </div>
           ) : (
-            <div className="columns-1 gap-4 sm:columns-2 lg:columns-3 2xl:columns-4">
+            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
               {latestCards.map((item) => (
                 <LatestCardTile key={item.streamId} item={item} />
               ))}
